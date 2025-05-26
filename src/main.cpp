5919cd7aad8ce22be359a6508cbd5c36bf833d39
Horia Mercan <horia.mercan@gmail.com>
2025-05-26 23:06:51 +0300

    it works with BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
-#include <BluetoothSerial.h>
 #include <map>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLEClient.h>
+
+#include <BLE2902.h>
 
 #include <TFT_eSPI.h> // Include the graphics library (this includes the sprite functions)
 #include <SPI.h>
@@ -20,6 +26,8 @@ byte state = 0;
 unsigned int colour = red << 11;
 
 uint8_t shouldRedrawMap = 0;
+bool formerDisplayMenu = false;
+
 
 // Check if Bluetooth Serial is properly supported
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -29,9 +37,17 @@ uint8_t shouldRedrawMap = 0;
 #define BAUD_RATE 9600
 
 // Global variables
-BluetoothSerial SerialBT;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
+
+struct device_connected_t
+{
+  esp_bd_addr_t remote_bda; // Remote Bluetooth device address
+  char name[10];
+};
+
+device_connected_t devices[2]; // Structure to hold connected device information
+uint32_t devices_size = 0;
 
 // Keep track of connected clients (in Classic BT we'll have just one active client)
 uint8_t connectedAddress[6] = {0};
@@ -42,6 +58,7 @@ bool hasConnectedClient = false;
 #define MAX_MESSAGE_LENGTH 100
 typedef struct
 {
+  uint32_t handle; // Handle of the characteristic
   uint16_t length;
   uint8_t data[MAX_MESSAGE_LENGTH];
 } message_t;
@@ -53,8 +70,14 @@ portMUX_TYPE messageQueueMux = portMUX_INITIALIZER_UNLOCKED;
 
 Minesweeper game;
 
-// Function to add message to queue to be handled by main loop
-bool addMessageToQueue(uint8_t *data, size_t length)
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+//--------------------------------------------START OF MESSAGE QUEUE CODE--------------------------------------------
+
+// Function to add message to queue to be handled fby main loop
+bool addMessageToQueue(uint32_t handle, uint8_t *data, size_t length)
 {
   if (length > MAX_MESSAGE_LENGTH)
   {
@@ -71,6 +94,7 @@ bool addMessageToQueue(uint8_t *data, size_t length)
     return false;
   }
 
+  messageQueue[messageQueueHead].handle = handle;
   messageQueue[messageQueueHead].length = length;
   memcpy(messageQueue[messageQueueHead].data, data, length);
 
@@ -96,44 +120,143 @@ bool getMessageFromQueue(message_t *message)
   return true;
 }
 
-// Bluetooth event callback
-void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+//--------------------------------------------END OF MESSAGE QUEUE CODE--------------------------------------------
+
+
+
+//--------------------------------------------START OF BLUETOOTH CONNECTION CODE--------------------------------------------
+
+class MyServerCallbacks : public BLEServerCallbacks
 {
-  if (event == ESP_SPP_SRV_OPEN_EVT)
+  void onConnect(BLEServer *pServer, esp_ble_gatts_cb_param_t *param)
   {
-    Serial.println("Client Connected");
-
     deviceConnected = true;
-    hasConnectedClient = true;
+    BLEDevice::startAdvertising();
+    // Get mac address of the connected device
+    esp_bd_addr_t *addr = (esp_bd_addr_t *)param->connect.remote_bda;
+    char mac[18];
+    snprintf(mac, sizeof(mac), "%02X:%02X:%02X:%02X:%02X:%02X",
+             (*addr)[0], (*addr)[1], (*addr)[2],
+             (*addr)[3], (*addr)[4], (*addr)[5]);
+    Serial.print("Connected to device with MAC: ");
+    Serial.println(mac);
 
-    // Store connected device address
-    memcpy(connectedAddress, param->srv_open.rem_bda, 6);
+    for (int i = 0; i < devices_size; i++)
+    {
+      if (memcmp(devices[i].remote_bda, addr, sizeof(esp_bd_addr_t)) == 0)
+      {
+        // Device already exists in the list
+        return;
+      }
+    }
+    if (devices_size < sizeof(devices) / sizeof(devices[0]))
+    {
+      // Add new device to the list
+      memcpy(devices[devices_size].remote_bda, addr, sizeof(esp_bd_addr_t));
 
-    char addrStr[18];
-    sprintf(addrStr, "%02X:%02X:%02X:%02X:%02X:%02X",
-            connectedAddress[0], connectedAddress[1], connectedAddress[2],
-            connectedAddress[3], connectedAddress[4], connectedAddress[5]);
-    Serial.print("Client address: ");
-    Serial.println(addrStr);
-    SerialBT.print("Hi from the server!");
-  }
+      char _name[10];
+      sprintf(_name, "Device %d", devices_size + 1);
+      size_t _name_len = strnlen(_name, 9);
+      strncpy(devices[devices_size].name, _name, _name_len);
+      devices[devices_size].name[_name_len] = '\0'; // Ensure null termination
+      devices_size++;
+      formerDisplayMenu = false; // Reset display menu flag
+    }
+    else
+    {
+      Serial.println("Device list is full, cannot add new device");
+    }
+  };
 
-  else if (event == ESP_SPP_CLOSE_EVT)
+  void onDisconnect(BLEServer *pServer, esp_ble_gatts_cb_param_t *param)
   {
-    Serial.println("Client Disconnected");
+    Serial.print("Device disconnected: ");
+    esp_bd_addr_t *addr = (esp_bd_addr_t *)param->connect.remote_bda;
+    char mac[18];
+    snprintf(mac, sizeof(mac), "%02X:%02X:%02X:%02X:%02X:%02X",
+             (*addr)[0], (*addr)[1], (*addr)[2],
+             (*addr)[3], (*addr)[4], (*addr)[5]);
+    Serial.println(mac);
     deviceConnected = false;
-    hasConnectedClient = false;
+
+    // Remove device from the list
+    for (int i = 0; i < devices_size; i++)
+    {
+      if (memcmp(devices[i].remote_bda, addr, sizeof(esp_bd_addr_t)) == 0)
+      {
+        // Shift remaining devices down
+        for (int j = i; j < devices_size - 1; j++)
+        {
+          memcpy(&devices[j], &devices[j + 1], sizeof(device_connected_t));
+        }
+        devices_size--;
+        formerDisplayMenu = false; // Reset display menu flag
+
+        break;
+      }
+    }
   }
 
-  else if (event == ESP_SPP_DATA_IND_EVT)
+  void onWrite(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param)
   {
-    // Data received
-    Serial.printf("Received %d bytes\n", param->data_ind.len);
-
-    // Add to message queue
-    addMessageToQueue(param->data_ind.data, param->data_ind.len);
+    std::string value = pCharacteristic->getValue();
+    if (value.length() > 0)
+    {
+      Serial.print("Received Value: ");
+      Serial.print(value.c_str());
+      Serial.printf(" From MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                    param->write.bda[0], param->write.bda[1],
+                    param->write.bda[2], param->write.bda[3],
+                    param->write.bda[4], param->write.bda[5]);
+      // Send back the received value
+      pCharacteristic->setValue(value);
+      pCharacteristic->notify();
+      Serial.println();
+      // Add to message queue
+      if (!addMessageToQueue(param->write.handle, (uint8_t *)value.c_str(), value.length()))
+      {
+        Serial.println("Message queue is full, dropping message");
+      }
+    }
   }
-}
+};
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+class MyCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param)
+  {
+    std::string value = pCharacteristic->getValue();
+    if (value.length() > 0)
+    {
+      Serial.print("Received Value: ");
+      for (int i = 0; i < value.length(); i++)
+      {
+        Serial.print(value[i]);
+      }
+      // Send back the received value
+      Serial.printf(" From MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                    param->write.bda[0], param->write.bda[1],
+                    param->write.bda[2], param->write.bda[3],
+                    param->write.bda[4], param->write.bda[5]);
+      // Send back the received value
+        // Add to message queue
+      if (!addMessageToQueue(param->write.handle, (uint8_t *)value.c_str(), value.length()))
+      {
+        Serial.println("Message queue is full, dropping message");
+      }
+      pCharacteristic->setValue(value);
+      pCharacteristic->notify();
+      Serial.println();
+    }
+  }
+};
+
+//--------------------------------------------END OF BLUETOOTH CONNECTION CODE--------------------------------------------
+
+
+//---------------------------------------------START OF ISRs CODE--------------------------------------------
 
 // ISR for pressing GPIO0 button
 void IRAM_ATTR buttonISR_GPIO0()
@@ -141,20 +264,24 @@ void IRAM_ATTR buttonISR_GPIO0()
   shouldRedrawMap = 1; // total reset the game
 }
 
+// Marjk as bomb button on GPIO2
 bool mark_as_bomb = false;
-
 void IRAM_ATTR buttonISR_GPIO2()
 {
   mark_as_bomb = true;
 }
 
-bool displayMenu = false;
+bool displayMenu = true; 
+
+// Handke Menu: Start game and pause on GPIO13
+void IRAM_ATTR buttonISR_GPIO13() {
+  displayMenu = !displayMenu; // Toggle menu display
+
+}
+
 
 int32_t lastButtonPress = 0;
-void IRAM_ATTR buttonISR_GPIO35()
-{
-  displayMenu = !displayMenu;
-}
+
 
 // https://circuitdigest.com/microcontroller-projects/esp32-timers-and-timer-interrupts
 // Setup one second timer
@@ -165,6 +292,10 @@ void IRAM_ATTR timerISR()
   timerCounter++;
 }
 
+//---------------------------------------------END OF ISRs CODE--------------------------------------------
+
+//---------------------------------------------START OF TFT DRAWING CODE--------------------------------------------
+
 void draw_map()
 {
   game.draw_map(tft);
@@ -174,13 +305,28 @@ void draw_menu()
 {
   tft.fillScreen(TFT_CYAN);
   tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(2);
+  tft.setTextSize(3);
   tft.setCursor(10, 10);
   tft.print("Menu");
-  tft.setTextSize(1);
-  tft.setCursor(10, 50);
-  tft.print("Press GPIO35 to resume");
+  tft.setTextSize(2);
+  tft.setCursor(3, 50);
+  tft.print("Press again to resume");
+
+  tft.setCursor(3, 70);
+  tft.print("Connected devices:");
+  for (int i = 0; i < devices_size; i++)
+  {
+    tft.setCursor(8, 90 + i * 20);
+    tft.printf("%s (%02X:%02X:%02X:%02X:%02X:%02X)",
+               devices[i].name,
+               devices[i].remote_bda[0], devices[i].remote_bda[1],
+               devices[i].remote_bda[2], devices[i].remote_bda[3],
+               devices[i].remote_bda[4], devices[i].remote_bda[5]);
+  }
+  
 }
+
+// ---------------------------------------------END OF TFT DRAWING CODE--------------------------------------------
 
 #include "pitches.h" // Include the pitches header for note definitions
 
@@ -207,15 +353,50 @@ int noteDurations[] = {
     8, 8, 8, 8,
     4, 4};
 
+void init_bt()
+{
+  // Create the BLE Device
+  BLEDevice::init("HoriaESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Add the callback for characteristic writes
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+}
+
 void setup()
 {
   Serial.begin(BAUD_RATE);
   Serial.println("Starting Bluetooth Classic Relay Server...");
 
-  // Initialize Bluetooth Serial
-  SerialBT.begin("HoriaESP32");
-  // SerialBT.connect()
-  SerialBT.register_callback(btCallback);
+  init_bt();
 
   Serial.println("Bluetooth Classic device started, ready to pair!");
 
@@ -236,8 +417,9 @@ void setup()
   pinMode(GPIO_NUM_2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(GPIO_NUM_2), buttonISR_GPIO2, FALLING);
 
-  // pinMode(GPIO_NUM_35, INPUT_PULLUP);
-  // attachInterrupt(digitalPinToInterrupt(GPIO_NUM_35), buttonISR_GPIO35, FALLING);
+  pinMode(GPIO_NUM_32, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(GPIO_NUM_32), buttonISR_GPIO13, FALLING);
+
 
   // Initialize timer for one second
   my_timer = timerBegin(0, 80, true);              // Timer 0, prescaler 80 (1 tick = 1 us)
@@ -317,13 +499,28 @@ void loop()
     }
   }
 
-  if (false)
+  message_t message;
+
+  if (displayMenu && !formerDisplayMenu)
   {
     Serial.println("Displaying menu");
     draw_menu();
+    formerDisplayMenu = true; // Set the flag to indicate menu is displayed
+    Serial.println("Menu button pressed, toggling displayMenu state");
+    Serial.printf("displayMenu: %d\n\n", displayMenu);
+    
   }
-  else
+  else if (displayMenu) {
+    
+    getMessageFromQueue(&message); // Loop over received messages while displaying the menu
+  }
+  else if (!displayMenu)
   {
+    if (formerDisplayMenu) {
+      tft.fillScreen(TFT_CYAN);
+      draw_map(); // Redraw the game map when exiting the menu
+      formerDisplayMenu = false; // Reset the flag when exiting the menu
+    }
     if (shouldRedrawMap)
     {
       tft.fillScreen(TFT_CYAN);
@@ -352,7 +549,6 @@ void loop()
       tft.print("You Won!");
       delay(200);
     }
-    message_t message;
     if (getMessageFromQueue(&message))
     {
       Serial.printf("Processing message (%d bytes)\n", message.length);
@@ -364,7 +560,7 @@ void loop()
       // If you want to echo back to the same device:
       if (deviceConnected)
       {
-        SerialBT.write(message.data, message.length);
+        // SerialBT.write(message.data, message.length);
       }
       if (message.data[0] == 'L')
       {
